18421bd9e80bd90fe412145230fa521e1d13ed85
yuritanu <fool0628@icloud.com>
2020-05-13 22:07:49 +0900

    freemaket_confimation_for_view

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,6 @@
+class ProductsController < ApplicationController
+
+  def edit
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'home#index'  
+  # root to: 'home#index'
+  root to: 'products#edit'  
   resources :users do
     collection do
       get 'new2'
       get 'new3'
     end
-  end  
+  end
+  resources :products, only: [:new, :edit]
 end
