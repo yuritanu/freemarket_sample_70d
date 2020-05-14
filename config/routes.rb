cb0394f85c18e3e6b3d79de2b28343dc3d5a0f31
niisan3211 <kappahaookikunaritai@icloud.com>
2020-05-14 19:33:53 +0900

    add:devise_routes
    routes.rbにdeviseを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root to: 'home#index'  
-  devise_for :users
-  resources :users do
-    collection do
-      get 'new2'
-      get 'new3'
-    end
-  end  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  
+  # resources :users do
+  #   collection do
+  #     get 'new2'
+  #     get 'new3'
+  #   end
+  # end  
   resources :products, only: :new
 end
