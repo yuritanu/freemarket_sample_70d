b7688eebd05cce6e2fcff932edf1ae2ab31222bf
niisan3211 <kappahaookikunaritai@icloud.com>
2020-05-15 23:33:02 +0900

    コメントアウト３

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  #deviseに必要な元々の認証キーは[email,password]だが、上のコードにより、認証キー(下にキーを記述)を追加することになる
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  # #deviseに必要な元々の認証キーは[email,password]だが、上のコードにより、認証キー(下にキーを記述)を追加することになる
 
   private
 
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name,:given_name,:family_name_kana,:given_name_kana,:birthday])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name,:given_name,:family_name_kana,:given_name_kana,:birthday])
+  # end
 end
