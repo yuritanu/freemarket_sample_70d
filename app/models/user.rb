ff2fe6899cb878999dca1cef8ca4795ae050c3e6
niisan3211 <kappahaookikunaritai@icloud.com>
2020-05-16 15:51:49 +0900

    コメントアウト外し

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ApplicationRecord
   validates :given_name_kana, presence: true
   validates :birthday, presence: true
 
-  # validate :password_complexity
-  # def password_complexity
-  #   return if password.blank? || password =~ /^(?=.*?[a-zA-Z])(?=.*?[0-9]).{7,70}$/
-  #   errors.add :password, "パスワードの強度が不足しています。パスワードの長さは7〜70文字とし、英字と数字をそれぞれ1文字以上含める必要があります。"
-  # end
+  validate :password_complexity
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[a-zA-Z])(?=.*?[0-9]).{7,70}$/
+    errors.add :password, "パスワードの強度が不足しています。パスワードの長さは7〜70文字とし、英字と数字をそれぞれ1文字以上含める必要があります。"
+  end
 
-  # has_one :profileaddress, dependent: :destroy
+  has_one :profileaddress, dependent: :destroy
 
 end
