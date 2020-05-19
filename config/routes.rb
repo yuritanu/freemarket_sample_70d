b7e8ef5cb97952c19362c4a3bade467fa96b3360
niisan3211 <kappahaookikunaritai@icloud.com>
2020-05-19 17:45:27 +0900

    add:routes
    deviseのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do 
-  # devise_for :users, controllers: {
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations'
-  # }
-  # devise_scope :user do
-  #   get  'profileaddresses', to: 'users/registrations#new_profileaddresses'
-  #   post 'profileaddresses', to: 'users/registrations#create_profileaddresses'
-  #   get  'deliveryaddresses',to: 'users/registrations#new_deliveryaddresses'
-  #   post 'deliveryaddresses', to: 'users/registrations#create_deliveryaddresses'
-
-  # end
-  root to: 'products#index' 
+  root to: 'products#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+  }
+  devise_scope :user do
+    get  '/profileaddresses', to: 'users/registrations#new_profileaddresses'
+    post '/profileaddresses', to: 'users/registrations#create_profileaddresses'
+    get  '/deliveryaddresses',to: 'users/registrations#new_deliveryaddresses'
+    post '/deliveryaddresses', to: 'users/registrations#create_deliveryaddresses'
+  end
+   
   resources :mypages, only: [:index] do
     collection do
       get 'logout'
@@ -18,22 +18,6 @@ Rails.application.routes.draw do
       get 'newcard'
     end
   end
-  
-  # ↓5/16スプリントレビュー後は削除
-  resources :notusers do
-    collection do
-      get 'new2'
-      get 'new3'
-    end
-  end  
-  # ↑5/16スプリントレビュー後は削除
 
-  resources :products, only: [:index, :new, :edit, :show] do
-    # 上記のproductsの最後にあるdoも5/16スプリントレビュー後は削除
-    # ↓も5/16スプリントレビュー用
-    collection do
-      get 'testshow'
-      get 'testconfirm'
-    end
-  end  
+  resources :products, only: [:index, :new, :edit, :show]
 end
