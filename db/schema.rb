1201d6c1725bcfa29fe1d84dfe4cf6540b240bef
niisan3211 <kappahaookikunaritai@icloud.com>
2020-06-17 20:44:15 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_134551) do
+ActiveRecord::Schema.define(version: 2020_05_29_095451) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
