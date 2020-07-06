a001b788451a3baa5dcdc45a7a77841b4e55d35d
yuritanu <fool0628@icloud.com>
2020-07-06 14:52:04 +0900

    三度目の正直

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,6 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'teamd70-yuritanu'
