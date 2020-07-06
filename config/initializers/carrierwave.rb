1a228bd9dab6aa980887e2c832c6190e3fcce00f
yuritanu <fool0628@icloud.com>
2020-07-06 13:55:19 +0900

    最終

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,24 +1,24 @@
-# require 'carrierwave/storage/abstract'
-# require 'carrierwave/storage/file'
-# require 'carrierwave/storage/fog'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
 
-# CarrierWave.configure do |config|
-#   if Rails.env.production?
-#     config.storage = :fog
-#     config.fog_provider = 'fog/aws'
-#     config.fog_credentials = {
-#       provider: 'AWS',
-#       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-#       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage = :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
 
-#       region: 'ap-northeast-1'
-#     }
-#     config.fog_directory  = 'teamd70'
-#     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/teamd70'
-#     config.fog_public = false
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'teamd70-yuritanu'    '
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/teamd70-yuritanu'
+    config.fog_public = false
 
-#   else
-#     config.storage :file # 開発環境:public/uploades下に保存
-#     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
-#   end  
-# end
+  else
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  end  
+end
